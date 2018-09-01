76d7b2817d8a6a66b92dab3db44a3c9ac562d3d1
Mohd Hilal Mehdi <m.hilalmehdi00@gmail.com>
2018-09-01 13:42:49 +0530

    First Commit

--- Multiple Linear Regression/newq.py
+++ Multiple Linear Regression/newq.py
@@ -8,17 +8,12 @@ from sklearn.preprocessing import LabelEncoder , OneHotEncoder
 label=LabelEncoder()
 X[: , 3]=label.fit_transform(X[: , 3])
 ohe=OneHotEncoder(categorical_features=[3])
-X=ohe.transform(X).toarray()
-X=X[:,1:]
+X=ohe.fit_transform(X).toarray()
+X=X[:,1:]#to avoid the dummy variable trap
 from sklearn.cross_validation import train_test_split
-X_train,Y_train,X_test,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
 regressor.fit(X_train,Y_train)
-results=regressor.predict(X_test)
-plt.scatter(X_train,Y_train,color="Red")
-plt.plot(X_train),regressor.predict(X_test),color="Green")
-plt.title("Salary with Mutiple Regression")
-plt.xlabel("Features")
-plt.ylabel('Salary')
+results_y=regressor.predict(X_test)
